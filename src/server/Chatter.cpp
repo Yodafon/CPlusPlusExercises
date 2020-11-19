22daccba9bc413736230a05f87e0fc1ff4c28a2a
László Kovács <yodafon@freemail.hu>
2020-11-19 21:38:41 +0100

    Add project files.

---
+++ src/server/Chatter.cpp
@@ -0,0 +1,12 @@
+﻿// Chatter.cpp : Defines the entry point for the application.
+//
+
+#include "server/Chatter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
