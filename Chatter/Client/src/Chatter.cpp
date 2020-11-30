799de1e5c1cf088d221815b08c35824d69e882de
László Kovács <yodafon@freemail.hu>
2020-11-30 22:45:56 +0100

    Restrucured files

---
+++ Chatter/Client/src/Chatter.cpp
@@ -0,0 +1,23 @@
+﻿// Chatter.cpp : Defines the entry point for the application.
+//
+
+#include "../header/Chatter.h"
+#include <boost/asio.hpp>
+using namespace std;
+using namespace boost::asio::ip;
+int main()
+{
+	string nickName, ipAddress;
+	cout << "Enter Nickname: ";
+	cin >> nickName;
+	cout << "Enter IP address: ";
+	cin >> ipAddress;
+	address address;
+	address.from_string(ipAddress);
+
+
+
+
+
+	return 0;
+}
---
+++ Chatter/Server/src/Chatter.cpp
@@ -0,0 +1,22 @@
+﻿// Chatter.cpp : Defines the entry point for the application.
+//
+
+#include "../header/Chatter.h"
+#include <boost/asio.hpp>
+using namespace std;
+using namespace boost::asio::ip;
+int main()
+{
+	boost::asio::io_context io_context;
+	tcp::socket socket(io_context);
+	tcp::endpoint endpoint;
+	address address;
+	address.from_string("127.0.0.1:80");
+	endpoint.address(address);
+	boost::system::error_code ec;
+	socket.connect(endpoint, ec);
+	cout << ec.message();
+
+
+	return 0;
+}
